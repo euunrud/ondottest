0861f6a09ada513d6cca48263370e80d725032a1
jung <wldud2550@naver.com>
2024-01-15 13:15:47 +0900

    ⚙️setting : spring boot 기본 설정

---
+++ src/main/java/com/ondot/ondot_back/OndotBackApplication.java
@@ -0,0 +1,13 @@
+package com.ondot.ondot_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OndotBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OndotBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ondot/ondot_back/OndotBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ondot.ondot_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OndotBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
